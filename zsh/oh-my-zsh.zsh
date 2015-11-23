4455dca1406656adfd9ef27b144450c7743f48ee
Rafael Eyng <rafaeleyng@gmail.com>
2015-11-23 19:48:09 -0200

    Add oh-my-zsh

---
+++ zsh/oh-my-zsh.zsh
@@ -0,0 +1,9 @@
+# Path to your oh-my-zsh installation.
+export ZSH=/Users/rafael/.oh-my-zsh
+
+ZSH_THEME="agnoster"
+DEFAULT_USER="rafael"
+
+plugins=(brew cloudapp extract git node npm)
+
+source $ZSH/oh-my-zsh.sh
