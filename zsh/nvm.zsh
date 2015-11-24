da4fbfb47c50fc82c41d2ed2ca38635b8b384c08
Rafael Eyng <rafaeleyng@gmail.com>
2015-11-23 22:22:55 -0200

    Fix nvm installation

--- zsh/nvm.zsh
+++ zsh/nvm.zsh
@@ -1,3 +1,16 @@
-# node version manager - requires
-[[ -s ~/.nvm/nvm.sh ]] && . ~/.nvm/nvm.sh
-nvm use stable > /dev/null
+# node version manager
+
+info () {
+  printf "\r  [ \033[00;34m..\033[0m ] $1\n"
+}
+
+if [ -s ~/.nvm/nvm.sh ]; then
+  source ~/.nvm/nvm.sh
+  nvm use stable > /dev/null
+else
+  info "Installing nvm"
+  curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.29.0/install.sh | bash
+  source ~/.nvm/nvm.sh
+  nvm install stable
+  nvm use stable
+fi
