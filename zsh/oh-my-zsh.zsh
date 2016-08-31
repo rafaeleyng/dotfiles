cb8b658b05872926fb5ec11dc143420393c21d50
Rafael Eyng <rafaeleyng@gmail.com>
2016-08-31 20:49:28 -0300

    add docker plugin

--- zsh/oh-my-zsh.zsh
+++ zsh/oh-my-zsh.zsh
@@ -4,6 +4,6 @@ export ZSH=/Users/rafael/.oh-my-zsh
 ZSH_THEME="agnoster"
 DEFAULT_USER="rafael"
 
-plugins=(brew cloudapp extract git node npm)
+plugins=(brew cloudapp extract git node npm docker)
 
 source $ZSH/oh-my-zsh.sh
