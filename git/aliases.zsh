4b83b507ad4da8f6ad636b02a64f94ff8a4fd4b3
Rafael Eyng <rafaeleyng@gmail.com>
2016-11-05 20:44:32 -0200

    organize git aliases

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -6,14 +6,21 @@ then
   alias git=$hub_path
 fi
 
-# The rest of my fun git aliases
-alias gl='git pull --prune'
-alias glog="git log --graph --pretty=format:'%Cred%h%Creset %an: %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr)%Creset' --abbrev-commit --date=relative"
-alias gp='git push origin HEAD'
-alias gd='git diff'
-alias gc='git commit'
-alias gca='git commit -a'
-alias gco='git checkout'
-alias gcb='git copy-branch-name'
+# read-only commands
+alias gs='git status -sb'
+alias gl="git log --graph --pretty=format:'%Cred%h%Creset %an: %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr)%Creset' --abbrev-commit --date=relative"
 alias gb='git branch'
-alias gs='git status -sb' # upgrade your git if -sb breaks for you. it's fun.
+alias gd='git diff'
+
+# write commands
+alias gbd='git branch -d'
+alias gbD='git branch -D'
+alias ga='git add .'
+alias gA='git add -A'
+alias gcm='git commit'
+alias gco='git checkout'
+alias gcb='git checkout -b'
+alias gps='git push origin HEAD'
+alias gpl='git pull --prune'
+alias gf='git fetch --prune'
+alias gcpb='git copy-branch-name'
