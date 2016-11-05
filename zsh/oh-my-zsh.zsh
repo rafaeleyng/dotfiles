7e31ab04f6a62b1393fe105d0818b5eeec819dba
Rafael Eyng <rafaeleyng@gmail.com>
2016-11-05 02:11:05 -0200

    add zsh autocomplete and syntax highlight and reset atom config

--- zsh/oh-my-zsh.zsh
+++ zsh/oh-my-zsh.zsh
@@ -4,6 +4,6 @@ export ZSH=/Users/rafael/.oh-my-zsh
 ZSH_THEME="agnoster"
 DEFAULT_USER="rafael"
 
-plugins=(brew cloudapp extract git node npm docker)
+plugins=(brew git node npm docker zsh-autosuggestions)
 
 source $ZSH/oh-my-zsh.sh
