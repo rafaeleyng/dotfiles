f60ba3ced3500692fcdd1c1510804a2adc74dc0e
Rafael Eyng <rafaeleyng@gmail.com>
2017-04-21 17:14:00 -0300

    fixing nvm

--- node/nvm.zsh
+++ node/nvm.zsh
@@ -1,5 +1,3 @@
 # node version manager
 
-# TODO rafael: doesn't work here, must be in the end of zshrc
-# export NVM_DIR="$HOME/.nvm"
-# [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh" # --no-use # --no-use for perfomance reasons:
+# TODO rafael: implement it here
