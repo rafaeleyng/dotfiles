2cc18ccdb935aa0110b17c19b38cb98619129e27
Rafael Eyng <rafaeleyng@gmail.com>
2017-04-21 17:07:32 -0300

    fixing nvm

--- node/install.sh
+++ node/install.sh
@@ -1,29 +1,13 @@
-node_install_nvm_and_node () {
-  local NVM_DIR=~/.nvm
-  local NVM_FILE=$NVM_DIR/nvm.sh
-  local NVM_VERSION=0.33.1
-  local NODE_VERSION=6.10.1
+export NVM_DIR="$HOME/.nvm"
 
-  echo "  Checking nvm installation"
+if [ ! -d $NVM_DIR ]; then
+  mkdir $NVM_DIR
+fi
 
-  if [ ! -s $NVM_FILE ]
-  then
-    echo "  Installing nvm $NVM_VERSION"
-    rm -fr $NVM_DIR
-    curl -o- https://raw.githubusercontent.com/creationix/nvm/v${NVM_VERSION}/install.sh | bash
-  fi
+[ -s "/usr/local/opt/nvm/nvm.sh" ] && . "/usr/local/opt/nvm/nvm.sh"
 
-  source $NVM_FILE
-
-  echo "  Installing node $NODE_VERSION"
-  nvm install $NODE_VERSION
-  nvm alias default $NODE_VERSION
-  nvm use default
-}
-
-node_install_global_dependencies () {
-  npm i -g diff-so-fancy
-}
-
-node_install_nvm_and_node
-node_install_global_dependencies
+echo "  Installing node $NODE_VERSION"
+NODE_VERSION=6.10.1
+nvm install $NODE_VERSION
+nvm alias default $NODE_VERSION
+nvm use default
--- node/nvm.zsh
+++ node/nvm.zsh
@@ -1,4 +1,5 @@
 # node version manager
 
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh" # --no-use # --no-use for perfomance reasons:
+# TODO rafael: doesn't work here, must be in the end of zshrc
+# export NVM_DIR="$HOME/.nvm"
+# [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh" # --no-use # --no-use for perfomance reasons:
