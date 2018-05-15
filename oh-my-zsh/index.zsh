c34715098d923d2a8785cbe62b1af89bf2f62632
Rafael Eyng <rafaeleyng@gmail.com>
2018-05-15 09:53:06 -0300

    wip

--- oh-my-zsh/index.zsh
+++ oh-my-zsh/index.zsh
@@ -7,3 +7,5 @@ plugins=(docker git yarn z)
 
 source $ZSH/oh-my-zsh.sh
 source /usr/local/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
+
+alias chrome="/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome"
