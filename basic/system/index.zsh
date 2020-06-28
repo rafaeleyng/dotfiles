0a85d3efa8a4cd831fbf4038b6d6658f2eaa674f
rafaeleyng <rafaeleyng@gmail.com>
2020-06-28 18:53:19 -0300

    add comment to debug

--- basic/system/index.zsh
+++ basic/system/index.zsh
@@ -1,7 +1,7 @@
 ######################################
 # variables
 ######################################
-export DOTFILES_DEBUG="0"
+export DOTFILES_DEBUG="0" # change to "1" to print debug information
 export PATH="./bin:/usr/local/bin:/usr/local/sbin:$DOTFILES/basic/bin:~/bin:$PATH"
 export EDITOR='code'
 export PROJECTS=~/code
