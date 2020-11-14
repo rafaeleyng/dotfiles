ce20cdc26afb829229d7864d5eadb85224231764
rafaeleyng <rafaeleyng@gmail.com>
2020-11-14 15:13:51 -0300

    add kubectl plugin

--- specific/oh-my-zsh/index.zsh
+++ specific/oh-my-zsh/index.zsh
@@ -3,7 +3,7 @@ export ZSH=~/.oh-my-zsh
 ZSH_THEME="powerlevel10k/powerlevel10k"
 DEFAULT_USER=$(whoami)
 
-plugins=(docker git z)
+plugins=(docker git kubectl z)
 
 source $ZSH/oh-my-zsh.sh
 source /usr/local/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
