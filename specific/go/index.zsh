ed9551098ed1eb3f65d29cf3206f1e91ca39e1b1
rafaeleyng <rafaeleyng@gmail.com>
2021-01-28 14:24:47 -0300

    add docker-compose oh-my-zsh plugin, add go-reshim for asdf

--- specific/go/index.zsh
+++ specific/go/index.zsh
@@ -1,3 +1,5 @@
 export GOPATH=$PROJECTS/go
 export GOBIN=$GOPATH/bin
 export PATH="$GOBIN:$PATH"
+
+alias go-reshim='asdf reshim golang && export GOROOT="$(asdf where golang)/go/"'
--- specific/oh-my-zsh/index.zsh
+++ specific/oh-my-zsh/index.zsh
@@ -3,7 +3,7 @@ export ZSH=~/.oh-my-zsh
 ZSH_THEME="powerlevel10k/powerlevel10k"
 DEFAULT_USER=$(whoami)
 
-plugins=(docker git kubectl z)
+plugins=(docker docker-compose git kubectl z)
 
 source $ZSH/oh-my-zsh.sh
 source /usr/local/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
