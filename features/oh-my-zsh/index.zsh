990b596de9519b22034cdeea7cbd124f06ff5865
rafaeleyng <rafaeleyng@gmail.com>
2022-12-25 15:43:06 -0300

    wip

--- features/oh-my-zsh/index.zsh
+++ features/oh-my-zsh/index.zsh
@@ -7,5 +7,5 @@ DEFAULT_USER=$(whoami)
 
 plugins=(docker docker-compose git z)
 
-source $ZSH/oh-my-zsh.sh
+source $ZSH/oh-my-zsh.sh # This is the slowest sourcing of the startup
 source /usr/local/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
