d912f12a421361513beb17e6733960c56c5dd47f
Rafael Eyng <rafaeleyng@gmail.com>
2023-07-28 22:04:16 -0300

    some TODOs I dont want to deal with right now

--- features/zsh/path.zsh
+++ features/zsh/path.zsh
@@ -1,3 +1,4 @@
 #!/bin/zsh
 
-export PATH="$DOTFILES/features/bin:~/bin:/usr/local/sbin:$PATH"
+# TODO don't use ~ on path: https://apple.stackexchange.com/questions/434805/zsh-does-not-find-script-in-path-while-bash-does-echo-path-is-the-same
+export PATH="$DOTFILES/features/bin:/home/rafael/bin:/usr/local/sbin:$PATH"
