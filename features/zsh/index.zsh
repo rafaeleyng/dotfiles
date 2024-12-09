3bd79b5fb3512c050c17ab4a7a4b27fe1687130f
Rafael Eyng <rafaeleyng@gmail.com>
2024-12-09 10:15:54 -0300

    trying to fix shell stuff

--- features/zsh/index.zsh
+++ features/zsh/index.zsh
@@ -14,27 +14,28 @@ HISTFILE=~/.zsh_history
 export HISTSIZE=1000000000
 export SAVEHIST=$HISTSIZE
 
-setopt NO_BG_NICE # don't nice background tasks
-setopt NO_HUP
-setopt NO_LIST_BEEP
-setopt LOCAL_OPTIONS # allow functions to have local options
-setopt LOCAL_TRAPS # allow functions to have local traps
-setopt HIST_VERIFY
-setopt SHARE_HISTORY # share history between sessions ???
-setopt EXTENDED_HISTORY # add timestamps to history
-setopt PROMPT_SUBST
-setopt CORRECT
-setopt COMPLETE_IN_WORD
-setopt IGNORE_EOF
+# TODO: I think these need to live in zshrc because of the whole subshell thing
+# setopt NO_BG_NICE # don't nice background tasks
+# setopt NO_HUP
+# setopt NO_LIST_BEEP
+# setopt LOCAL_OPTIONS # allow functions to have local options
+# setopt LOCAL_TRAPS # allow functions to have local traps
+# setopt HIST_VERIFY
+# setopt SHARE_HISTORY # share history between sessions ???
+# setopt EXTENDED_HISTORY # add timestamps to history
+# setopt PROMPT_SUBST
+# setopt CORRECT
+# setopt COMPLETE_IN_WORD
+# setopt IGNORE_EOF
 
-setopt APPEND_HISTORY # adds history
-setopt INC_APPEND_HISTORY SHARE_HISTORY  # adds history incrementally and share it across sessions
-setopt HIST_IGNORE_ALL_DUPS  # don't record dupes in history
-setopt HIST_REDUCE_BLANKS
+# setopt APPEND_HISTORY # adds history
+# setopt INC_APPEND_HISTORY SHARE_HISTORY  # adds history incrementally and share it across sessions
+# setopt HIST_IGNORE_ALL_DUPS  # don't record dupes in history
+# setopt HIST_REDUCE_BLANKS
 
-# don't expand aliases _before_ completion has finished
-#   like: git comm-[tab]
-setopt complete_aliases
+# # don't expand aliases _before_ completion has finished
+# #   like: git comm-[tab]
+# setopt complete_aliases
 
 bindkey '^[^[[D' backward-word
 bindkey '^[^[[C' forward-word
