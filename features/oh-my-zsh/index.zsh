0140b75766a1be23c008b58a3d7d3319179a1ba8
rafaeleyng <rafaeleyng@gmail.com>
2025-01-27 04:19:50 -0800

    update installation

--- features/oh-my-zsh/index.zsh
+++ features/oh-my-zsh/index.zsh
@@ -5,6 +5,6 @@ export ZSH=~/.oh-my-zsh
 ZSH_THEME="powerlevel10k/powerlevel10k"
 DEFAULT_USER=$(whoami)
 
-plugins=(asdf docker-compose git z)
+plugins=(asdf docker-compose git macos z)
 
 source $ZSH/oh-my-zsh.sh # This is the slowest sourcing of the startup
